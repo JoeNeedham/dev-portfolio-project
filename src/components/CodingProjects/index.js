b5210624768fc4a1b32a265b9d47481086ed90f9
joeneedum <joeneedum@gmail.com>
2022-06-16 21:50:29 -0400

    commit

--- src/components/CodingProjects/index.js
+++ src/components/CodingProjects/index.js
@@ -4,8 +4,15 @@ import './codingprojects.css'
 function CodingProjects() {
     return (
         <div className="projects-container">
-            <div className="section-title">
-                <h1>RECENT PROJECTS</h1>
+            <div className="project-section">
+                <div className="section-title">
+                    <h1>RECENT PROJECTS</h1>
+                </div>
+                <div className="projects">
+                    <div className="box"></div>
+                    <div className="box"></div>
+                    <div className="box"></div>
+                </div>
             </div>
         </div>
     )
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -9,7 +9,16 @@ function HomePage() {
         <div className='home-container'>
             <div className='home-content'>
                 <h1 className='job-title'>FRONTEND DEVELOPER</h1>
-                <h2 className='adjectives'>CREATIVE - DESIGNER - PROBLEM SOLVER </h2>
+            <div className="attributes">
+                <h2 style={{color: "red"}} className='adjectives'>CREATIVE - </h2>
+                &nbsp;
+                &nbsp;
+                <h2 style={{color: "yellow"}} className='adjectives'>DESIGNER - </h2>
+                &nbsp;
+                &nbsp;
+                <h2 style={{color: "blue"}} className='adjectives'>PROBLEM SOLVER</h2>
+            </div>
+                {/* <h2 className='adjectives'>CREATIVE - DESIGNER - PROBLEM SOLVER </h2> */}
             </div>
         </div>
     )
