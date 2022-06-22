b7630a78cdf58f5c52174720c44f2191313c6a11
joeneedum <joeneedum@gmail.com>
2022-06-21 23:57:14 -0400

    about-done

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -10,6 +10,32 @@ function AboutMe() {
                 </div>
                 <div className="about-content">
                     <h2>Education - Central Michigan University (Winter 2017), Coding Dojo (Fall 2021)</h2>
+                    <br />
+                    <h2>Interested In - Web Design, UX, and CSS</h2>
+                    <br />
+                    <h2>Currently Learning - TypeScript, Sass, and Figma</h2>
+                    
+                    <div className="about-tech">
+                        <h2>Technologies Used</h2>
+                        <div className="tech-pictures">
+                            <div className='tech-box'>
+                                <img src="images/react.png" alt="" />
+                                <p>REACT</p>
+                            </div>
+                            <div className='tech-box'>
+                                <img src="images/redux.png" alt="" />
+                                <p>REDUX</p>
+                            </div>
+                            <div className='tech-box'>
+                                <img src="images/node.png" alt="" />
+                                <p>NODE.JS</p>
+                            </div>
+                            <div className='tech-box'>
+                                <img src="images/mongoDB.png" alt="" />
+                                <p>MongoDB</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
