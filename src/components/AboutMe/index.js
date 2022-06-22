7bb16350b1b67f99b85cb9200c2c311920aa7bdd
joeneedum <joeneedum@gmail.com>
2022-06-21 23:06:07 -0400

    commit

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,8 +4,13 @@ import './aboutme.css'
 function AboutMe() {
     return (
         <div className='about-container'>
-            <div className="about-title">
-                <h1>HEY, I'M JOE!</h1>
+            <div className="about-section">
+                <div className="about-title">
+                    <h1>HEY, I'M JOE!</h1>
+                </div>
+                <div className="about-content">
+                    <h2>Education - Central Michigan University (Winter 2017), Coding Dojo (Fall 2021)</h2>
+                </div>
             </div>
         </div>
     )
