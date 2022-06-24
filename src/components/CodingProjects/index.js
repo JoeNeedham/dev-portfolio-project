f7e9a61da87ee280d623f15390d3b9b53c542d50
joeneedum <joeneedum@gmail.com>
2022-06-23 21:36:16 -0400

    update

--- src/components/CodingProjects/index.js
+++ src/components/CodingProjects/index.js
@@ -12,12 +12,15 @@ function CodingProjects() {
                     <div className="box">
                         {/* <span style={{ color: "white" }}>Portfolio Website</span> */}
                         <video autoPlay muted loop src="media/RIOVIISUALS - 23 June 2022.mp4"></video>
+                        <span style={{  margin: "10px 0 10px 10px", color: "white"}}>Portfolio Website</span>
                     </div>
                     <div className="box">
                         <video autoPlay muted loop src="media/Netflix - 23 June 2022.mp4"></video>
+                        <span style={{  margin: "10px 0 10px 10px", color: "white"}}>Netflix Clone</span>
                     </div>
                     <div className="box">
                         <video autoPlay muted loop src="media/Anime Watch List - 23 June 2022.mp4"></video>
+                        <span className='video-title' style={{ margin: "10px 0 10px 10px",color: "white"}}>Anime Watch List</span>
                     </div>
                 </div>
             </div>
