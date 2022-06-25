e47e5a8ef677c3089d8f473fad276f25c022b258
joeneedum <joeneedum@gmail.com>
2022-06-24 22:50:39 -0400

    commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,22 @@
-import { React, useState } from 'react'
+import { React, useState, useRef } from 'react'
 import { AboutMe, HomePage, ContactMe, CodingProjects} from '../../components'
 import { Navbar } from '../../components'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import classNames from 'classnames'
 import './home.css'
 
 function Home() {
 
     const [isClicked, setIsClicked] = useState(false)
+
+    // const aboutMeRef = useRef()
+
+    // const handleClick = () => {
+    //     console.log("working")
+    //     console.log(aboutMeRef)
+
+    //     aboutMeRef.current.scrollIntoView({ behavior: 'smooth' })
+    // }
 
     const handleToggle = () => {
         setIsClicked(!isClicked);
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './navbar.css'
 
-function Navbar() {
+function Navbar(props) {
+
+
     return (
         <div className="nav">
             <div className="nav_content">
                 <h2 className='name'><span><i className="fa-brands fa-react react-icon"></i></span>JOE NEEDHAM</h2>
                 <div className="nav_links_content">
                     <ul className="nav_link_list">
-                        <li><a href="#" className="nav_link active">home</a></li>
+                        <li><a className="nav_link active">home</a></li>
                         <li><a className="nav_link">About Me</a></li>
                         <li><a className="nav_link">Portfolio</a></li>
                         <li><a className="nav_link">Contact</a></li>
