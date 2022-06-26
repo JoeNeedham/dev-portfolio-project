602c7bb0d42050bbee73730f26a5b5ffe8b55dde
joeneedum <joeneedum@gmail.com>
2022-06-25 20:07:50 -0400

    commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,16 +7,7 @@ import './home.css'
 
 function Home() {
 
-    const [isClicked, setIsClicked] = useState(false)
-
-    // const aboutMeRef = useRef()
-
-    // const handleClick = () => {
-    //     console.log("working")
-    //     console.log(aboutMeRef)
-
-    //     aboutMeRef.current.scrollIntoView({ behavior: 'smooth' })
-    // }
+    const [isClicked, setIsClicked] = useState(false);
 
     const handleToggle = () => {
         setIsClicked(!isClicked);
@@ -27,14 +18,9 @@ function Home() {
         // "bkg-light": !isClicked,
     });
 
-    const onClickHandler = () => {
-        document.getElementById("aboutme").scrollIntoView();
-    }
-
-
     return (
         <div className={wrapper}>
-            <Navbar onclickhandler={onClickHandler} />
+            <Navbar />
             <HomePage />
             <AboutMe />
             <CodingProjects />
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,68 @@
-import React from 'react'
+import { React, useState} from 'react'
 import './navbar.css'
+import classNames from 'classnames';
 
-function Navbar(props) {
+function Navbar() {
 
     const homeNav = () => {
         // document.getElementById("aboutme").scrollIntoView();
         window.scrollTo({ top: 0, left: 0, behavior: "smooth"} );
+        handleToggle()
+
     }
 
     const aboutMeNav = () => {
         // document.getElementById("aboutme").scrollIntoView();
         window.scrollTo({ top: 700, left: 0, behavior: "smooth"} );
+        handleToggle()
     }
 
     const portfolioNav = () => {
         // document.getElementById("aboutme").scrollIntoView();
         window.scrollTo({ top: 1300, left: 0, behavior: "smooth"} );
+        handleToggle()
+
     }
 
     const contactNav = () => {
         // document.getElementById("aboutme").scrollIntoView();
         window.scrollTo({ top: 2100, left: 0, behavior: "smooth"} );
+        handleToggle()
     }
+
+
+    const [isClicked, setIsClicked] = useState(true);
+
+    const handleToggle = () => {
+        if(!isClicked){
+            setIsClicked(!isClicked);
+        }
+        else {
+            setIsClicked(isClicked);
+        }
+
+    }
+
+    const navLink1 = classNames('nav_link', {
+
+        'nav_link active': isClicked,
+        'nav_link': !isClicked
+    });
+
+    const navLink2 = classNames('nav_link', {
+        'nav_link active': isClicked,
+        'nav_link': !isClicked
+    });
+
+    const navLink3 = classNames('nav_link', {
+        'nav_link active': isClicked,
+        'nav_link': !isClicked
+    });
+
+    const navLink4 = classNames('nav_link', {
+        'nav_link active': isClicked,
+        'nav_link': !isClicked
+    });
 
 
 
@@ -31,10 +72,10 @@ function Navbar(props) {
                 <h2 className='name'><span><i className="fa-brands fa-react react-icon"></i></span>JOE NEEDHAM</h2>
                 <div className="nav_links_content">
                     <ul className="nav_link_list">
-                        <li><a onClick={homeNav} className="nav_link active">Home</a></li>
-                        <li><a onClick={aboutMeNav} className="nav_link">About Me</a></li>
-                        <li><a onClick={portfolioNav}  className="nav_link">Portfolio</a></li>
-                        <li><a onClick={contactNav} className="nav_link">Contact</a></li>
+                        <li><a onClick={homeNav} className={navLink1} >Home</a></li>
+                        <li><a onClick={aboutMeNav} className={navLink1} >About Me</a></li>
+                        <li><a onClick={portfolioNav} className={navLink1} >Portfolio</a></li>
+                        <li><a onClick={contactNav} className={navLink1}>Contact</a></li>
                     </ul>
                 </div>
             </div>
